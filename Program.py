aade1b8faa11871f40763cb87cc44af4509cd6c5
ZuggRugg <shayne.tman@gmail.com>
2025-02-23 16:00:43 -0600

    update

--- Program.py
+++ Program.py
@@ -1,47 +1,37 @@
-# Shayne Tieman CS325 Project_1
-# Use the conda env function that is now installed with the conda.el 
-# package use pipeline function that is included with transformers library
+# -----------------------------------------------------------------------
+# Shayne Tieman CS325 Project_1                                         |
+# Section 1                                                             | 
+# Receive list of responses from two LLM's                              |
+# -----------------------------------------------------------------------
 
-# https://huggingface.co/docs/transformers/en/autoclass_tutorial
-
-import transformers
-# from transformers import pipeline 
-# pipeline, AutoTokenizer
-print("\nMy current version of Transformers:", transformers.__version__)
+import transformers as trans
+# print("\nMy current version of Transformers:", trans.__version__)
 
 
-# need to run a pretrained model that is loaded from the package otherwise wont work. 
-# Tokenizer converts a string to an input you can insert to the model
-# It may also be the case that I am wrong on all this as well (RTFM)
-
-
-# function to read data from text file
-# create variable to store the prompts inside of a list. 
+# Data Class :: reads the 'input.txt' and appends it to a list 
 class data:
-    input_list = []
+    def __init__(self):
+        self.input_list = []
+        #make append function for list of input data
 
     def read_data():
-        f = open("input.txt", "r")
+        f = open("input.txt", "r", encoding="UTF-8")
         data_read = f.read()
         print("\n", data_read)
         f.close()
+        
+
+# Model Class :: Contains Information about the current two Prompted Models and Tokenizers used
+class models:
+    model1 = "distilgpt2"
+    model2 = "different model"
+
+    def response(self):
+        generator = trans.pipeline(task="text-generation", model="distilgpt2", tokenizer="distilgpt2")
+        x = generator("Hi how are you?")
+        print(x)
 
 
-
-# model class that would be used to get two textgen models and read input list. 
-# class models:
-# transcriber = pipeline(task="automatic-speech-recognition")
-
-
-
-data.read_data()
-
-
-
-    
-
-
-
-
-
-
+data.read_data() #Read out the current Input file
+new = models() 
+new.response() #generate common response using GPT2
--- README.md
+++ README.md
@@ -2,14 +2,18 @@
 
 This is the first Project for CS-325, Utilzing two LLM's to read input text from a file and generate a response.
 
+I used Python 3.13.2 and the most up to date version of the Transformers module for Python, for the library that Transformers utilizes' I chose Pytorch
+
 
 # Requirements
 
-I used Python 3.13.2 and the most up to date version of the Transformers module for Python, for the library that Transformers utilizes' I chose Pytorch
+Download the requirements.yaml and install for an environment of your choosing using Venv or Miniconda 
 
-Download the requirements.yaml and install for an environment of your choosing
+```python3
+pip install requirements.yaml
+```
 
 
 # Misc / Explanation
 
-The two Large Language Models I chose were 
+The two Large Language Models I chose were GPT2 and Ollama
