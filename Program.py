1d5593e162f6bfdb2852921377ad80c223970318
ZuggRugg <shayne.tman@gmail.com>
2025-02-25 13:57:57 -0600

    made input list, Updated README

--- Program.py
+++ Program.py
@@ -1,15 +1,15 @@
 # -----------------------------------------------------------------------
 # Shayne Tieman CS325 Project_1                                         |
-# Section 1                                                             | 
+# Section 1                                                             |
 # Receive list of responses from two LLM's                              |
 # -----------------------------------------------------------------------
 
 import transformers as t
 # print("\nMy current version of Transformers:", trans.__version__)
 
-# Data Class :: reads the 'input.txt' and appends it to a list 
+# Data Class :: reads the 'input.txt' and appends it to a list
 class data:
-    def __init__(self): 
+    def __init__(self):
         self.input_list = [] # where the file contents are stored in the list
 
 
@@ -19,30 +19,29 @@ class data:
         self.input_list = data_read.split("\n\n")
         # print("\n", self.input_list) just a test in case file reading not working
         f.close()
-        
+
 
 # Model Class :: Contains Information about the current two Prompted Models and Tokenizers used
 class models:
     def __init__(self):
         self.model1 = t.AutoModelForCausalLM.from_pretrained('distilbert/distilgpt2')
         self.tokenizer1 = t.AutoTokenizer.from_pretrained('distilbert/distilgpt2')
-        # model2 = "different model"
+        # self.model2 = t.AutoModelForCausalLM.from_pretrained('')
+        # self.tokenizer2 = t.AutoTokenizer.from_pretrained('')
 
     def response(self):
-        generator = t.pipeline(task="text-generation", model=self.model1, tokenizer=self.tokenizer1)
-        x = generator("Hi how are you? My name is Jack!")
+        generator1 = t.pipeline(task="text-generation", model=self.model1, tokenizer=self.tokenizer1)
+        x = generator1("Hi how are you? My name is Jack!")
+        
         print(x)
 
 
 # Read Data from Text file and append to a list :: data.input_list
 Data = data()
 Data.read_data("input.txt")
+input_list = Data.input_list
 
 print("\n")
 
 model_instance = models() 
 model_instance.response() #generate common response using GPT2 and Llama 
-
-
-
-
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This is the first Project for CS-325, Utilzing two LLM's to read input text from a file and generate a response.
 
-I used Python 3.13.2 and the most up to date version of the Transformers module for Python, for the library that Transformers utilizes' I chose Pytorch
+I used Python 3.13.2 and the most up to date version of the Transformers module for Python, for the library that Transformers utilize's I chose Pytorch
 
 
 # Requirements
@@ -12,8 +12,16 @@ Download the requirements.yaml and install for an environment of your choosing u
 ```python3
 pip install requirements.yaml
 ```
+then to compile go over to the working directory and use the command
 
+```python3
+python Program.py
+```
 
 # Misc / Explanation
 
 The two Large Language Models I chose were GPT2 and Ollama
+
+If you're running this on smaller laptops its possible that you might get a memory error, If that is the case then
+your computer might not be able to run the modle. The GPT2 model should only need <1 GB of RAM
+
