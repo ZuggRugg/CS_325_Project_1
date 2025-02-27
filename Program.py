72198e70978e671c48cd0f284b931efcdeaf8db8
ZuggRugg <shayne.tman@gmail.com>
2025-02-27 16:14:32 -0600

    working prototype

--- Program.py
+++ Program.py
@@ -36,20 +36,25 @@ class models:
         self.output_list = []
         self.model1 = t.AutoModelForCausalLM.from_pretrained('distilbert/distilgpt2')
         self.tokenizer1 = t.AutoTokenizer.from_pretrained('distilbert/distilgpt2')
+        self.tokenizer2 = t.AutoTokenizer.from_pretrained('HuggingFaceTB/SmolLM2-360M-Instruct')
+        self.model2 = t.AutoModelForCausalLM.from_pretrained('HuggingFaceTB/SmolLM2-360M-Instruct')
 
     def gpt2response(self, input_list):
-            generator1 = t.pipeline(task="text-generation", model=self.model1, tokenizer=self.tokenizer1)
+            generator1 = t.pipeline(task="text-generation", model=self.model1, tokenizer=self.tokenizer1, torch_dtype=torch.bfloat16, device_map="auto")
             for i in range(3):
                 x = generator1(input_list[i])
-                print('gpt2 output: ', x, '\n')
-                self.output_list.append(x)
+                fstring = f"gpt2 output: {x}\n"
+                print(fstring)
+                self.output_list.append(fstring)
 
 
-    def TinyLlama_response(self, input_list):
-        self.model2 = t.AutoModelForCausalLM.from_pretrained('TinyLlama/TinyLlama-1.1B-Chat-v1.0')
-        generator2 = t.pipeline(task="text-generation", model=self.model1)
-        x = generator1(input_list[1])
-        print('TinyLlama output: ', x, '\n')
+    def SmallLM_response(self, input_list):
+        for i in range(3):
+            generator2 = t.pipeline(task="text-generation", model=self.model2, tokenizer=self.tokenizer2, torch_dtype=torch.bfloat16, device_map="auto")
+            x = generator2(input_list[i])
+            fstring = f"SmallLM output: {x}\n"
+            print(fstring)
+            self.output_list.append(fstring)
 # End of Class ---------------------------------------------------------------
 
 
@@ -59,8 +64,11 @@ Data.read_data("input.txt")
 input_list = Data.input_list
 
 
-model_instance = models() 
-model_instance.gpt2response(input_list) #generate common response using GPT2 
-contents = model_instance.output_list
+model_instance = models()
+model_instance.gpt2response(input_list) # Generate some output with SmallLM (360 Million Parameters)
+model_instance.SmallLM_response(input_list) # Generate common response using GPT2 (128 Million Parameters)
+output = model_instance.output_list
 
-Data.write_data('output.txt', contents)
+Data.write_data('output.txt', output)
+
+
