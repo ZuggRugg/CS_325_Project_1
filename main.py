766d5700ad08faaf41660aa0d8ab01f3fc0427fc
ZuggRugg <shayne.tman@gmail.com>
2025-04-07 09:08:56 -0500

    added requirements file

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
-import soup
+import requests
+from bs4 import BeautifulSoup
 
-##TODO: get beautful soup package for Python in webScrapping env
+# TODO: get beautful soup package for Python in webScrapping env
+# TODO: pip install bs4, then put that into a requirements.yaml
 
-print("hello")
+
+# class get_Data():
+    
+
+URL = "http://www.values.com/inspirational-quotes"
+r = requests.get(URL)
+print(r.content)
+
+soup = BeautifulSoup(r.content, 'html.parser')
+print(soup.)
+
+
+# print(soup.prettify())
+
+
+print("program terminated!")
