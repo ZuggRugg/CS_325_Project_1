3ceea65f5d369657a5015c779d932454920eff3b
ZuggRugg <shayne.tman@gmail.com>
2025-04-08 14:20:45 -0500

    added input, output, README work

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # TODO:
-TODO: get beautful soup package for Python in webScrapping env
+make classes and find appropriate website to scrape
 
 # Introduction
-
+This Project deals with implementing a web scrapping library and using it to grab information from local news websites
 
 # Technology Used
-
+I used Python 3.13.3 with the Beautful Soup Package to parse and scrape HTML documents 
 
 # Screenshots
--- main.py
+++ main.py
@@ -3,20 +3,31 @@ from bs4 import BeautifulSoup
 
 # TODO: get beautful soup package for Python in webScrapping env
 # TODO: pip install bs4, then put that into a requirements.yaml
+# TODO: Get two more local business links and get data from the websites
+
 
 
 # class get_Data():
+# URL = ["https://www.bizjournals.com/stlouis/"]
+
+# class WebSrapper():
+
     
+
 
 URL = "http://www.values.com/inspirational-quotes"
 r = requests.get(URL)
 print(r.content)
 
 soup = BeautifulSoup(r.content, 'html.parser')
-print(soup.)
+# print(soup.)
 
 
 # print(soup.prettify())
 
 
 print("program terminated!")
+
+
+
+
